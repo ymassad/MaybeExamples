9beec2632c2080a6aa91e7f67dae8b76b16a49ab
Yacoub Massad <yacoubmassad@gmail.com>
2019-06-01 16:48:54 +0300

    Working on MaybeAsAStruct

---
+++ MaybeAsAStruct/Maybe.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MaybeAsAStruct
+{
+    public struct Maybe<T>
+    {
+        private readonly T value;
+
+        private readonly bool hasValue;
+
+        private Maybe(T value)
+        {
+            this.value = value;
+            hasValue = true;
+        }
+
+        public TResult Match<TResult>(Func<T, TResult> some, Func<TResult> none)
+        {
+            if (hasValue)
+                return some(value);
+
+            return none();
+        }
+
+        public void Match(Action<T> some, Action none)
+        {
+            if (hasValue)
+            {
+                some(value);
+            }
+            else
+            {
+                none();
+            }
+        }
+
+        public static implicit operator Maybe<T>(T value)
+        {
+            if(value == null)
+                return new Maybe<T>();
+
+            return new Maybe<T>(value);
+        }
+
+        public static implicit operator Maybe<T>(Maybe.MaybeNone value)
+        {
+            return new Maybe<T>();
+        }
+
+        public bool TryGetValue(out T value)
+        {
+            if (hasValue)
+            {
+                value = this.value;
+                return true;
+            }
+
+            value = default(T);
+            return false;
+        }
+    }
+
+    public static class Maybe
+    {
+        public class MaybeNone
+        {
+        }
+
+        public static MaybeNone None { get; } = new MaybeNone();
+    }
+}
---
+++ MaybeAsAStruct/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace MaybeAsAStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Test2()
+        {
+            var contents = GetLogContents(1);
+
+            if (contents.TryGetValue(out var value))
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Log file not found");
+            }
+        }
+
+        static void Test3()
+        {
+            var contents = GetLogContents(1);
+
+            contents.Match(some: value =>
+            {
+                Console.WriteLine(value);
+            },
+            none: () =>
+            {
+                Console.WriteLine("Log file not found");
+            });
+        }
+
+        static Maybe<string> GetLogContents(int id)
+        {
+            var filename = "c:\\logs\\" + id + ".log";
+
+            if (File.Exists(filename))
+                return File.ReadAllText(filename);
+
+            return Maybe.None;
+        }
+    }
+}
